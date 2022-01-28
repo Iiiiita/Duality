f0bf42fcbb3c727f9d6b52727d6e30b999bde5fe
JyTero <71631096+JyTero@users.noreply.github.com>
2022-01-28 21:48:07 +0200

    Kokeellista

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float movementSpeed = 5;
     // Start is called before the first frame update
     void Start()
     {
