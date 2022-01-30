f8c4e3a2d54f4b273771490ba23783b4a11c0479
Iiiiita <iideeaa@hotmail.com>
2022-01-30 10:09:52 +0200

    Main menu pitäisi olla kunnossa, ääniasetusten toimia joka scenessä ja lisätty settings nappi myös leveleille

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject gameOverPanel;
     public GameObject victoryPanel;
+    public GameObject settingsPanel;
+ 
     // Start is called before the first frame update
     void Start()
     {
         gameOverPanel.SetActive(false);
         if (victoryPanel != null)
         { victoryPanel.SetActive(false); }
+        settingsPanel.SetActive(false);
+    }
+
+    public void Settings()
+    {
+        settingsPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ClosePanel()
+    {
+        settingsPanel.SetActive(false);
+        Time.timeScale = 1;
     }
 
 
